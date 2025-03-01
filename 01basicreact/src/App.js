672804618ff517941ad5e0bed6db29c2698843cd
chaitalee <132096020+chaitalee19@users.noreply.github.com>
2025-03-01 10:50:17 +0000

    #React project setup

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+<h1>HELLO WORLD | Chaitalee</h1>
+  );
+}
+
+export default App;
---
+++ 01vite_react/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+   <h1>hello world with vite | CG</h1>
+  )
+}
+
+export default App
